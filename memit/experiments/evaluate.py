4acbd9da7937fcf68e7c0f6c50467cd7b6260481
herrdai <1450398241@qq.com>
2026-05-06 21:58:44 +0800

    fix(evaluate): update model path to 138 server (remove xzw1)

--- memit/experiments/evaluate.py
+++ memit/experiments/evaluate.py
@@ -232,7 +232,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--model_name",
         choices=["gpt2-medium", "gpt2-large", "gpt2-xl", "EleutherAI/gpt-j-6B",
-                 "/home/wentao/xzw1/LLM/Meta-Llama-3-8B-Instruct", "/home/wentao/xzw1/LLM/my_llama-3-8b-instruct"],
+                 "/home/wentao/LLM/Meta-Llama-3-8B-Instruct", "/home/wentao/LLM/my_llama-3-8b-instruct"],
         default="gpt2-xl",
         help="Model to edit.",
         required=True,
